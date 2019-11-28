e9b696ffb74379287b8d579915a5623ec60aa739
nightness <joshguyette@yahoo.com>
2019-11-28 16:54:31 -0600

    clean-up commit

--- build-dist.sh
+++ build-dist.sh
@@ -1,57 +1,51 @@
 #!/bin/bash
 
 function install_deps(){
+    echo -e "\033[1;33m[*] Installing package dependencies...\033[1;32m"
     sudo apt-get remove libimobiledevice-utils libimobiledevice-dev
     sudo apt-get install git libtool m4 automake autoconf libxml2-dev python2.7-dev libusb-dev \
         libusb-1.0-0-dev libssl-dev libreadline-dev libplist-dev libplist++-dev libplist++3v5 libplist-utils
+    echo -e "\033[1;33m[*] Package dependencies installed"
 }
 
 function build_libimobiledevice(){
     libs=( "libplist" "libusbmuxd" "libimobiledevice" "libirecovery" "idevicerestore" )
-    #libs=( "libplist" "libusbmuxd" "libimobiledevice" )
-    #libs=( "idevicerestore" )
     
     buildlibs() {
         for i in "${libs[@]}"
         do
-            echo -e "\033[1;32mFetching $i..."
+            echo -e "\033[1;33m[*] Fetching $i...\033[1;32m"
             git clone https://github.com/libimobiledevice/${i}.git
             cd $i
-            echo -e "\033[1;32mConfiguring $i..."
+            echo -e "\033[1;33m[*] Configuring $i...\033[1;32m"
             autoreconf --install
             ./autogen.sh
             if [ "${i}" = "libimobiledevice" ]; then
-                echo -e "\033[1;32mPatching $i..."
+                echo -e "\033[1;33m[*] Patching $i...\033[1;32m"
                 patch -i ../../patches/cython.patch ./cython/debugserver.pxi > /dev/null 2> /dev/null
             fi
-            echo -e "\033[1;32mBuilding $i..."
-            if [[ -e $(make) ]]; then
-                echo -e "\033[1;32mInstalling $i..."
-                sudo make install
-            fi
+            echo -e "\033[1;33m[*] Building $i...\033[1;32m"
+            make > /dev/null
+            echo -e "\033[1;33m[*] Installing $i...\033[1;32m"
+            sudo make install > /dev/null
             cd ..
         done
     }
     
+    echo -e "\033[1;33m[*] Building and installing libplist, libusbmuxd, libimobiledevice, libirecovery, idevicerestore..."
     # Create a new dist folder
     if [ -d `pwd`/dist ]; then
-        echo "[*] Removing old dist directory"
+        echo -e "\033[1;31m[*] Removing old dist directory"
         rm -rf ./dist
     fi
-    echo "[*] Creating new dist directory"
+    echo -e "\033[1;33m[*] Creating new dist directory"
     mkdir dist
     cd dist
     buildlibs
-    if [[ -e $(which ldconfig) ]]; then
-        sudo ldconfig
-    fi
+    sudo ldconfig &>/dev/null
     cd ..
+    echo -e "\033[1;33m[*] Finished"
 }
 
-echo "[*] Installing package dependencies..."
 install_deps
-echo "[*] Package dependencies installed"
-
-echo "[*] Building and installing libplist, libusbmuxd, libimobiledevice, libirecovery, idevicerestore..."
 build_libimobiledevice
-echo "[*] Finished"
